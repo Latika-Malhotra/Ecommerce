75581d1d5d18c7cded4a58975c4fb5a00b9f036c
latika malhotra <latika.malhotra@tothenew.com>
2021-08-02 05:03:25 +0530

    Initial Commit

---
+++ ECommerce/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿
+using Core.Entitites;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int Id)
+        {
+
+            var products = await _context.Products.FindAsync(Id);
+            return Ok(products);
+        }
+    }
+
+
+}
